44e2a87c065321d7fb6d6855b4eca65d0d0332aa
IlyasMuhammad <dalimarilyas@gmail.com>
2018-11-15 12:17:44 +0700

    React js shopping cart

---
+++ src/data/products.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: 1,
+    name: 'Jagung',
+    currency: 'Rp',
+    price: 10000,
+   
+    image: 'images/06.jpg',
+  },
+  
+  {
+    id: 4,
+    name: 'Corn',
+    price: 20000,
+    currency: 'Rp',
+    image: 'images/06.jpg',
+  },
+ 
+  {
+    id: 5,
+    name: 'Jagung segar',
+    price: 16000,
+    currency: 'Rp',
+    image: 'images/06.jpg',
+  },
+
+];
+
+export default data;
